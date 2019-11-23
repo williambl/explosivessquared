7d99189ef78a52d0296f704de974db66d37860dc
williambl <william@williambl.com>
2019-11-23 16:41:50 +0000

    Add a list of explosive builders used to make explosives

--- src/main/kotlin/com/williambl/explosivessquared/ExplosivesSquared.kt
+++ src/main/kotlin/com/williambl/explosivessquared/ExplosivesSquared.kt
@@ -2,9 +2,13 @@ package com.williambl.explosivessquared
 
 import net.alexwells.kottle.KotlinEventBusSubscriber
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
+import net.minecraft.block.material.Material
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.world.Explosion
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.client.registry.RenderingRegistry
@@ -24,7 +28,16 @@ object ExplosivesSquared {
     val entityTypesToBlocks: MutableMap<EntityType<out ExplosiveEntity>, ExplosiveBlock> = mutableMapOf()
     val blocksToEntityTypes: MutableMap<ExplosiveBlock, EntityType<out ExplosiveEntity>> = mutableMapOf()
 
-    lateinit var explosive: ExplosiveBuilder
+    var explosives: List<ExplosiveBuilder> = listOf(
+            ExplosiveBuilder("big_tnt").setExplodeFunction { it.world.createExplosion(it, it.posX, it.posY, it.posZ, 15f, Explosion.Mode.DESTROY) },
+            ExplosiveBuilder("slow_tnt")
+                    .setFuseLength(160)
+                    .setExplodeFunction { it.world.createExplosion(it, it.posX, it.posY, it.posZ, 4f, Explosion.Mode.DESTROY) },
+            ExplosiveBuilder("cake_tnt")
+                    .setBlockProperties(Block.Properties.create(Material.CAKE))
+                    .setItemProperties(Item.Properties().group(ItemGroup.FOOD))
+                    .setExplodeFunction { it.world.setBlockState(it.position, Blocks.CAKE.defaultState) }
+    )
 
     @SubscribeEvent
     public fun setup(event: FMLCommonSetupEvent) {
@@ -41,23 +54,22 @@ object ExplosivesSquared {
 
     @SubscribeEvent
     fun registerBlocks(event: RegistryEvent.Register<Block>) {
-        explosive = ExplosiveBuilder("explosive")
-        event.registry.register(
-                explosive.createBlock()
+        event.registry.registerAll(
+                *explosives.map { it.createBlock() }.toTypedArray()
         )
     }
 
     @SubscribeEvent
     fun registerItems(event: RegistryEvent.Register<Item>) {
-        event.registry.register(
-                explosive.createItem()
+        event.registry.registerAll(
+                *explosives.map { it.createItem() }.toTypedArray()
         )
     }
 
     @SubscribeEvent
     fun registerEntityTypes(event: RegistryEvent.Register<EntityType<out Entity>>) {
-        event.registry.register(
-                explosive.createEntityType()
+        event.registry.registerAll(
+                *explosives.map { it.createEntityType() }.toTypedArray()
         )
     }
 
