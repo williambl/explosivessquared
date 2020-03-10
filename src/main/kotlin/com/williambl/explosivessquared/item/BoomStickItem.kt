71b5c44169d074642873493f03dd4754618a78f8
williambl <william@williambl.com>
2020-03-10 18:32:23 +0000

    Make boomstick explosions happen inside the block they're focused on

--- src/main/kotlin/com/williambl/explosivessquared/item/BoomStickItem.kt
+++ src/main/kotlin/com/williambl/explosivessquared/item/BoomStickItem.kt
@@ -18,7 +18,7 @@ class BoomStickItem(val explosiveType: ExplosiveType, properties: Item.Propertie
     override fun onItemRightClick(world: World, playerIn: PlayerEntity, handIn: Hand): ActionResult<ItemStack> {
         val result = rayTrace(playerIn, 64.0, world)
         if (result.type == RayTraceResult.Type.BLOCK) {
-            val explosiveEntity = ExplosiveEntity(explosiveType.entityType, world, result.hitVec.x, result.hitVec.y, result.hitVec.z, playerIn)
+            val explosiveEntity = ExplosiveEntity(explosiveType.entityType, world, result.pos.x.toDouble(), result.pos.y.toDouble(), result.pos.z.toDouble(), playerIn)
             explosiveEntity.setFuse(0)
             world.addEntity(explosiveEntity)
             world.playSound(null as PlayerEntity?, explosiveEntity.posX, explosiveEntity.posY, explosiveEntity.posZ, SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0f, 1.0f)
