db3109d0140be19bece6817ec5fa30435d9c0c5d
williambl <william@williambl.com>
2020-03-15 14:23:26 +0000

    Add support for filters in BlockActionManager

--- src/main/kotlin/com/williambl/explosivessquared/util/actions/BlockActionManager.kt
+++ src/main/kotlin/com/williambl/explosivessquared/util/actions/BlockActionManager.kt
@@ -3,18 +3,26 @@ package com.williambl.explosivessquared.util.actions
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import net.minecraft.block.BlockState
 import net.minecraft.util.concurrent.ThreadTaskExecutor
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.fml.LogicalSide
 import net.minecraftforge.fml.LogicalSidedProvider
+import java.util.function.Supplier
 
 class BlockActionManager(val world: World, val positions: Sequence<BlockPos>) {
 
     private val actions: MutableList<BlockAction> = mutableListOf()
+    private val filters: MutableList<(World, BlockPos, BlockState) -> Boolean> = mutableListOf()
 
     public fun addAction(action: BlockAction): BlockActionManager {
         actions.add(action)
+        return this
+    }
+
+    public fun addFilter(filter: (World, BlockPos, BlockState) -> Boolean): BlockActionManager {
+        filters.add(filter)
         return this
     }
 
@@ -22,16 +30,23 @@ class BlockActionManager(val world: World, val positions: Sequence<BlockPos>) {
         launch {
             val executor = LogicalSidedProvider.WORKQUEUE.get<ThreadTaskExecutor<in Runnable>>(LogicalSide.SERVER)
             val runnables = mutableListOf<Runnable>()
+            val suppliers = mutableListOf<Supplier<Boolean>>()
             positions.forEach { pos ->
-                actions.forEach {
-                    runnables.add(Runnable { if (it.matches(world, pos, world.getBlockState(pos))) it.process(world, pos) })
-                    executor.runAsync {
-                        runnables.forEach {
-                            it.run()
-                        }
-                    }.await()
-                    runnables.clear()
+                filters.forEach {
+                    suppliers.add(Supplier { it(world, pos, world.getBlockState(pos)) })
                 }
+                if (suppliers.isEmpty() || executor.supplyAsync { suppliers.all { it.get() } }.await()) {
+                    actions.forEach {
+                        runnables.add(Runnable { if (it.matches(world, pos, world.getBlockState(pos))) it.process(world, pos) })
+                        executor.runAsync {
+                            runnables.forEach {
+                                it.run()
+                            }
+                        }.await()
+                        runnables.clear()
+                    }
+                }
+                suppliers.clear()
             }
         }
     }
