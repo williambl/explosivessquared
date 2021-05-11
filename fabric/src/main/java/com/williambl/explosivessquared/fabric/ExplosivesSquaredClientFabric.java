27fddbc7e455a7a196ff42880c9531c8c415b3a2
williambl <william@williambl.com>
2021-05-11 20:40:25 +0100

    Fix potential concurrentmodification when spawning entity on fabric

--- fabric/src/main/java/com/williambl/explosivessquared/fabric/ExplosivesSquaredClientFabric.java
+++ fabric/src/main/java/com/williambl/explosivessquared/fabric/ExplosivesSquaredClientFabric.java
@@ -16,12 +16,14 @@ public class ExplosivesSquaredClientFabric implements ClientModInitializer {
         ExplosivesSquaredClient.INSTANCE.initClient();
         ClientPlayNetworking.registerGlobalReceiver(new ResourceLocation("explosivessquared:spawn"), (client, handler, buf, responseSender) -> {
             if (client.world != null) {
-                Entity entity = Registry.ENTITY_TYPE.getByValue(buf.readVarInt()).create(client.world);
-                if (entity != null) {
-                    client.world.addEntity(buf.readInt(), entity);
-                    entity.setUniqueId(buf.readUniqueId());
-                    entity.setPositionAndRotation(buf.readDouble(), buf.readDouble(), buf.readDouble(), buf.readFloat(), buf.readFloat());
-                }
+                client.enqueue(() -> {
+                    Entity entity = Registry.ENTITY_TYPE.getByValue(buf.readVarInt()).create(client.world);
+                    if (entity != null) {
+                        client.world.addEntity(buf.readInt(), entity);
+                        entity.setUniqueId(buf.readUniqueId());
+                        entity.setPositionAndRotation(buf.readDouble(), buf.readDouble(), buf.readDouble(), buf.readFloat(), buf.readFloat());
+                    }
+                });
             }
         });
     }
